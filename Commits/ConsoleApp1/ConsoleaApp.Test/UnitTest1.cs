fd5d9520a302b61af6bfb89b3874159ece747b4e
Raoul Holzer <rh@hotmail.com>
2022-06-03 14:20:57 +0200

    Add Test for #1 PrintGroupBuildingQuestion

--- Commits/ConsoleApp1/ConsoleaApp.Test/UnitTest1.cs
+++ Commits/ConsoleApp1/ConsoleaApp.Test/UnitTest1.cs
@@ -1,11 +1,53 @@
+using System.Xml.Serialization;
+
 namespace ConsoleaApp.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetQuestionIfGroupCountOrMemberCountGroupBuilding()
         {
+            Printer printer = new Printer();
+            var groupBuilder = new GroupBuilder(printer);
+            groupBuilder.PrintGroupBuildingQuestion();
+           
+            Assert.Equal("Soll nach Gruppenanzahl oder Mitgliederanzahl die Gruppen gebildet werden?", printer.Lines.Last());
+        }
 
+
+    }
+
+    class Printer
+    {
+        public List<string> Lines = new List<string>();
+
+        public void WriteLine(string text)
+        {
+            Lines.Add(text);
+        }
+
+    }  
+    
+    class ConsolePrinter 
+    {
+        private List<string> Lines = new List<string>();
+
+        public void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+    }
+
+    internal class GroupBuilder
+    {
+        public GroupBuilder(Printer printer)
+        {
+        }
+
+        internal void PrintGroupBuildingQuestion()
+        {
+            throw new NotImplementedException();
         }
     }
 }
