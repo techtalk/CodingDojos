b638c81419629599882b0a41e3ad54c4f46b68f5
Manuel Juran <mju@corp.techtalk.at>
2022-09-15 16:19:43 +0200

    added initial version of ppt, empty sln

---
+++ CupcakeDecorator/CupcakeDecorator/CupcakeDecorator.Test/CupcakeTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace CupcakeDecorator.Test;
+
+public class CupcakeTests
+{
+    [Fact]
+    //The name function should return “🧁”
+    public void Cupcake_Name_ReturnsCupcake()
+    {
+        //arrange
+
+
+        //act
+
+
+        //assert
+
+    }
+
+    [Fact]
+    //The price function should return 1$ for “🧁”
+    public void Cupcake_Price_ReturnsCupcake()
+    {
+        //arrange
+
+
+        //act
+
+
+        //assert
+
+    }
+}
